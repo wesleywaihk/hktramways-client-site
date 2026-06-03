a239e7757ba26f21f338f8c4b3936d56107035a1
Wesley Wai <wesley.wai@gmail.com>
2026-06-03 21:04:50 +0900

    initialize strapi

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,5 +1,19 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: env('UPLOAD_PROVIDER', 'local'),
+      ...(env('UPLOAD_PROVIDER') === 'aws-s3' && {
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: { Bucket: env('AWS_BUCKET') },
+        },
+      }),
+    },
+  },
+});
 
 export default config;
